656e8c0b1beec8b5f62b22e1af6ca865852b2d4a
pavithrarani <pavithra.be08@gmail.com>
2019-11-16 21:41:00 +0530

    Problem1: Find a pair in a given array using 1D array and sorting. Enhanced to O(n) using unordered_map

--- Datastructures/Datastructures.cpp
+++ Datastructures/Datastructures.cpp
@@ -82,26 +82,26 @@ void pairForGivenSum(int arr[], int sum,int n)
 	}
 
 }
-//int main()
-//{
-//	cout << "hello world";
-//	int sum;
-//	int arr[] = {8,7,2,5,3,1  };
-//	int n = sizeof(arr) / sizeof(arr[0]);
-//	cout << " Find a pair with given sum in the Array" << endl;
-//	cout << "size of array is" << n << endl;
-//	for (int i = 0;i < n ;i++)
-//	{
-//		cout << arr[i] << endl;
-//
-//	}
-//	cout << "sum =";
-//	cin >> sum;
-//
-//	pairForGivenSum(arr,sum,n);
-//
-//	getchar();
-//	return 0;
-//}
-//
+int main()
+{
+	cout << "hello world";
+	int sum;
+	int arr[] = {8,7,2,5,3,1  };
+	int n = sizeof(arr) / sizeof(arr[0]);
+	cout << " Find a pair with given sum in the Array" << endl;
+	cout << "size of array is" << n << endl;
+	for (int i = 0;i < n ;i++)
+	{
+		cout << arr[i] << endl;
+
+	}
+	cout << "sum =";
+	cin >> sum;
+
+	pairForGivenSum(arr,sum,n);
+
+	getchar();
+	return 0;
+}
+
 
